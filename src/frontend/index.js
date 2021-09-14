4516c02a78aefe0ab10def519a549741c9dc1c47
Eric Campbell <ericcampbell59@gmail.com>
2021-09-14 11:16:53 -0500

    update package

--- src/frontend/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ConfigProvider, useConfig } from './use-config';
-export { default as UserProvider, UserContext, useUser } from './use-user';
-export { default as withPageAuthRequired } from './with-page-auth-required';
-//# sourceMappingURL=index.js.map
